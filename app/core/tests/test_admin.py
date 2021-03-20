af2201b7d15976513610a4c7b0cbf3d10a8429ff
wancat <wancat@pm.me>
2021-03-20 23:31:46 +0800

    Add custom User Admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class AdminSiteTest(TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
@@ -21,7 +21,7 @@ class AdminSiteTest(TestCase):
 
     def test_users_listed(self):
         """Test if users are listed on the admin site"""
-        url = reverse('admin:core_user_changellist')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
