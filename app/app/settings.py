36408cf4e500d6619ba34be31b204dce2f0a3fb5
wancat <wancat@pm.me>
2021-04-18 17:12:51 +0800

    Wait for db command

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ('DB_HOST'),
-        'NAME': os.environ('DB_NAME'),
-        'USER': os.environ('DB_USER'),
-        'PASSWORD': os.environ('DB_PASS'),
+        'HOST': os.environ['DB_HOST'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
     }
 }
 
